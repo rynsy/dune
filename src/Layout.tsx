850b61058e1acf14bb450a013e0627611428dbbf
Ryan Lindsey <ryan.lindsey44@gmail.com>
2024-10-09 18:57:51 -0400

    changing title

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
   return (
     <div>
-      <h1>Sand and Fluid Simulation</h1>
+      <h1>sands</h1>
       {children}
     </div>
   );
