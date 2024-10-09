5b3918ecc59ad106d2019fe941e6814739e76032
Ryan Lindsey <ryan.lindsey44@gmail.com>
2024-10-09 18:52:31 -0400

    setting up gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  base: '/personal_site/', // Replace with your actual repository name
 });
